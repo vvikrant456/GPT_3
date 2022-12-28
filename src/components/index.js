c4f1ef9abcd20d95ffd92d1384a3fa6fa6aea9ba
vvikrant456 <vvikrant456@gmail.com>
2022-12-28 21:08:59 +0530

    gpt

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/brand';
+export { default as CTA } from './cta/Cta';
+export { default as Features } from './feature/Features';
+export { default as Navbar } from './navbar/navbar';
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from './blog/blog';
+export { default as Feature } from './features/Feature';
+export { default as Footer } from './Footer/footer';
+export { default as Header } from './Header/header';
+export { default as Possibility } from './possibility/Possibility';
+export { default as WhatGpt3 } from './whatgpt3/whatgpt3';
